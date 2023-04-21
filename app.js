fd1f0980853e17a1d16ca282497a6363058a7957
IamHetPatel <hetp943@gmail.com>
2023-04-21 20:19:12 +0530

    .env

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const followerRoutes = require("./routes/followerRoutes");
 const mongoose = require("mongoose");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017', {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
